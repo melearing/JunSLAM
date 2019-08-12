c97e546055ce926576da662dfc453775d790df77
melearing <meliangwenjun@foxmail.com>
2019-08-12 12:52:27 +0800

    19812 从GXclone下源码，并针对G2O、OpenCV的更新进行修改

---
+++ build/src/CMakeFiles/myslam.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lee/storehouse/svn/base-slam/src/camera.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/camera.cpp.o"
+  "/home/lee/storehouse/svn/base-slam/src/config.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/config.cpp.o"
+  "/home/lee/storehouse/svn/base-slam/src/frame.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/frame.cpp.o"
+  "/home/lee/storehouse/svn/base-slam/src/g2o_types.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/g2o_types.cpp.o"
+  "/home/lee/storehouse/svn/base-slam/src/map.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/map.cpp.o"
+  "/home/lee/storehouse/svn/base-slam/src/mappoint.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/mappoint.cpp.o"
+  "/home/lee/storehouse/svn/base-slam/src/visual_odometry.cpp" "/home/lee/storehouse/svn/base-slam/build/src/CMakeFiles/myslam.dir/visual_odometry.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/home/lee/software/Sophus"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
